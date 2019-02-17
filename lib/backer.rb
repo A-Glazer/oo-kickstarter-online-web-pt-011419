1b3c3e832ca1b15e21f4c2bbeaae46eefeda5ca9
A Glazer <avigayilglazer@gmail.com>
2019-02-17 14:49:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,11 +10,11 @@ class Backer
   def back_project(project)
     @backed_projects << project
     # add = Project.new(backed_projects)
-<<<<<<< HEAD
+
      project.add_backer(self)
-=======
+
     project.add_backer(self)
->>>>>>> 4a9ad4a8d76e072e2de379254e41bb598ec59bea
+
 
   end
 
